ebde71d7d1c3e760a52e1fce81c52dc64ddfb210
Pedro Endo <endopedro@gmail.com>
2020-12-29 11:58:11 -0300

    adjustments

--- js/app.js
+++ js/app.js
@@ -1,77 +1,79 @@
-const canvas = document.getElementById("canvas")
-const ctx = canvas.getContext("2d")
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-let particulas = []
+let particles = [];
 
-const NUM_PARTICULAS = 100
-const TAM_PARTICULAS = 20
-const COR_INFECTADA = "red"
+const PARTICLES_NUM = 200;
+const PARTICLES_SIZE = 10;
+const INFECTED_COLOR = "red";
 
-class Particula {
-	constructor(cor) {
-		this.x = canvas.width * Math.random()
-		this.y = canvas.height * Math.random()
-		this.vx = 12 * Math.random() - 6
-		this.vy = 12 * Math.random() - 6
-		this.Color = cor
-	}
+class Particle {
+  constructor(cor) {
+    this.x = canvas.width * Math.random();
+    this.y = canvas.height * Math.random();
+    this.vx = 12 * Math.random() - 6;
+    this.vy = 12 * Math.random() - 6;
+    this.Color = cor;
+  }
 
-	mudarCor(cor) {
-		this.Color = cor
-	}
+  changeColor(cor) {
+    this.Color = cor;
+  }
 
-	draw(ctx) {
-		ctx.fillStyle = this.Color
-		ctx.fillRect(this.x, this.y, TAM_PARTICULAS, TAM_PARTICULAS)
-	}
+  draw(ctx) {
+    ctx.fillStyle = this.Color;
+    ctx.fillRect(this.x, this.y, PARTICLES_SIZE, PARTICLES_SIZE);
+  }
 
-	update() {
-		this.x += this.vx
-		this.y += this.vy
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
 
-		if (this.x<0 || this.x+TAM_PARTICULAS > canvas.width)
-			this.vx = -this.vx
+    if (this.x < 0 || this.x + PARTICLES_SIZE > canvas.width)
+      this.vx = -this.vx;
 
-		if (this.y < 0 || this.y+TAM_PARTICULAS > canvas.height)
-			this.vy = -this.vy
+    if (this.y < 0 || this.y + PARTICLES_SIZE > canvas.height)
+      this.vy = -this.vy;
 
-		particulas.forEach(particula => {
-			if(this != particula) {
-				let distanciaX = Math.abs(particula.x - this.x);
-				let distanciaY = Math.abs(particula.y - this.y);
-				let distancia = Math.sqrt(Math.pow(distanciaX, 2) + Math.pow(distanciaY, 2));
-				if(distancia < TAM_PARTICULAS) {
-					this.vx = -this.vx
-					this.vy = -this.vy
+    particles.forEach((particle) => {
+      if (this != particle) {
+        let distanceX = Math.abs(particle.x - this.x);
+        let distanceY = Math.abs(particle.y - this.y);
+        let distance = Math.sqrt(
+          Math.pow(distanceX, 2) + Math.pow(distanceY, 2)
+        );
+        if (distance < PARTICLES_SIZE) {
+          this.vx = -this.vx;
+          this.vy = -this.vy;
 
-					if(particula.Color == COR_INFECTADA) this.mudarCor(COR_INFECTADA)
-				}
-			}
-		})
-	}
+          if (particle.Color == INFECTED_COLOR)
+            this.changeColor(INFECTED_COLOR);
+        }
+      }
+    });
+  }
 }
 
 const loop = () => {
-	ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	for (var i = 0; i < NUM_PARTICULAS; i++) {
-		particulas[i].update()
-		particulas[i].draw(ctx)
-	}
+  for (var i = 0; i < PARTICLES_NUM; i++) {
+    particles[i].update();
+    particles[i].draw(ctx);
+  }
 
-	requestAnimationFrame(loop)
-}
+  requestAnimationFrame(loop);
+};
 
 const init = () => {
-	for (var i = 0; i < NUM_PARTICULAS; i++)
-		particulas.push(new Particula("white"))
+  for (var i = 0; i < PARTICLES_NUM; i++) particles.push(new Particle("white"));
 
-	particulas[0].mudarCor(COR_INFECTADA)
+  particles[0].changeColor(INFECTED_COLOR);
 
-	loop()
-}
+  loop();
+};
 
-init()
+init();
